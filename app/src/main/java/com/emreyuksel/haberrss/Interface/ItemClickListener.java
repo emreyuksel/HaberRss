e12b87c28666c3d1463906d0a647fc691ecb1bfc
emreyuksel <emreyuksel7@gmail.com>
2017-09-12 16:55:43 +0300

    HaberRSS

---
+++ app/src/main/java/com/emreyuksel/haberrss/Interface/ItemClickListener.java
@@ -0,0 +1,12 @@
+package com.emreyuksel.haberrss.Interface;
+
+import android.view.View;
+
+/**
+ * Created by Emre on 12.9.2017.
+ */
+
+public interface ItemClickListener {
+    void onClick(View view, int position, boolean isLongClick);
+
+}
---
+++ app/src/main/java/com/emreyuksel/haberrss/Model/Feed.java
@@ -0,0 +1,66 @@
+package com.emreyuksel.haberrss.Model;
+
+/**
+ * Created by Emre on 12.9.2017.
+ */
+
+public class Feed
+{
+    public String url;
+    public String title;
+    public String link;
+    public String author;
+    public String description;
+
+
+    public Feed(String url, String title, String link, String author, String description) {
+        this.url = url;
+        this.title = title;
+        this.link = link;
+        this.author = author;
+        this.description = description;
+
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+}
